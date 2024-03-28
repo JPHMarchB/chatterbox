19906725ad3db3ae4e08e812de7a67ede33fee04
Joseph Marchbanks <153705879+JPHMarchB@users.noreply.github.com>
2024-03-27 21:09:24 -0600

    Added layout for not found page

---
+++ frontend/src/app/(not-found)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import '../globals.css'
+import {Navigation} from "@/app/shared/Navigation";
+import {SideBar} from "@/app/shared/SideBar";
+
+
+export const metadata: Metadata = {
+    title: 'Chatterbox',
+    description: 'description goes here',
+}
+
+type RootLayoutProps = {
+    children: React.ReactNode
+}
+
+export default function RootLayout(props : RootLayoutProps) {
+    const { children } = props
+    return (
+        <html  lang="en" suppressHydrationWarning>
+        <body className='bg-black'>
+        <Navigation/>
+        {children}
+        </body>
+        </html>
+    )
+}
