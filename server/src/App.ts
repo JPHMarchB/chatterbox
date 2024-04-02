c3ae07a0359a70f83a1c05e560d35aff972c609d
Joseph Marchbanks <153705879+JPHMarchB@users.noreply.github.com>
2024-04-02 14:22:33 -0600

    Added sign out API to project

--- server/src/App.ts
+++ server/src/App.ts
@@ -10,6 +10,7 @@ import {likeRoute} from "./apis/like/like.route";
 import {signUpRoute} from "./apis/sign-up/sign-up.route";
 import {imageRoute} from "./apis/image/image.route";
 import {signInRoute} from "./apis/sign-in/sign-in.route";
+import {signOutRoute} from "./apis/sign-out/sign-out.route";
 
 
 // The following class creates the app and instantiates the server
@@ -59,6 +60,7 @@ export class App {
         this.app.use(indexRoute.basePath, indexRoute.router)
         this.app.use(signUpRoute.basePath, signUpRoute.router)
         this.app.use(signInRoute.basePath, signInRoute.router)
+        this.app.use(signOutRoute.basePath, signOutRoute.router)
         this.app.use(profileRoute.basePath, profileRoute.router)
         this.app.use(imageRoute.basePath, imageRoute.router)
         this.app.use(likeRoute.basePath, likeRoute.router)
