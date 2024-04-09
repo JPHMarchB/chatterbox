ac9b745b4e3ffc8d287f31657d5a0a0071eab3e6
Joseph Marchbanks <153705879+JPHMarchB@users.noreply.github.com>
2024-04-09 14:34:03 -0600

    Created view for the explore page to display posts, needs spacing fixes

--- frontend/src/app/(main-site)/explore/Explore.tsx
+++ frontend/src/app/(main-site)/explore/Explore.tsx
@@ -1,25 +1,27 @@
 import {JSX} from "react";
+import {Post} from "@/utils/models/post.model";
+import {fetchAllPosts} from "@/utils/http/post.http";
 
-function ExplorePost() : JSX.Element {
+type Props = {
+    post : Post
+}
+
+function ExplorePost(props: Props) : JSX.Element {
+    const {post} = props
+
     return (
         <>
-            <img className='image-full' src='/profile-placeholder.jpg' alt='Post'/>
+            <img className='image-full' src={post.postImageUrl} alt='Post'/>
         </>
     )
 }
 
-export function Explore() : JSX.Element {
+export async function Explore() : Promise<JSX.Element> {
+    const posts = await fetchAllPosts()
     return (
         <>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1'>
-                <ExplorePost/>
-                <ExplorePost/>
-                <ExplorePost/>
-                <ExplorePost/>
-                <ExplorePost/>
-                <ExplorePost/>
-                <ExplorePost/>
-                <ExplorePost/>
+                {posts.map((post:Post) => <ExplorePost post={post}/>)}
             </div>
         </>
     )
