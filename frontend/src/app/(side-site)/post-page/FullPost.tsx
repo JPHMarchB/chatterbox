b29b20ad601b7ae1fd670958a80a009c04a012c8
Joseph Marchbanks <153705879+JPHMarchB@users.noreply.github.com>
2024-04-10 21:59:41 -0600

    Improved view for individual posts

--- frontend/src/app/(side-site)/post-page/FullPost.tsx
+++ frontend/src/app/(side-site)/post-page/FullPost.tsx
@@ -12,19 +12,39 @@ export async function FullPost(props:Props) : Promise<JSX.Element> {
 
     return (
         <>
-            <main className='bg-[#222] h-[100vh] flex justify-center'>
-                <div className='border-x-2'>
-                    <section className='p-5 bg-black'>
+            <main className='bg-[#222] backdrop-blur flex justify-center'>
+                <div>
+                    <section className='p-4 bg-black'>
                         <a href="../"><img src='/arrow-left.svg' alt='Back button'/></a>
                     </section>
 
-                    <section>
-                        <img className='border-y-2' src={post.postImageUrl} alt='Post image'/>
+                    <section className='bg-[#111111] border-t border-gray-800'>
+                        <div className='flex items-center gap-4 p-4 text-white'>
+                            {profile.profileImageUrl && <img className='rounded-full w-10 h-10' src={profile.profileImageUrl} alt='User profile image'/>}
+                            <h2>{profile.profileName}</h2>
+                        </div>
                     </section>
 
-                    <section className='flex items-center bg-black p-5 border-2'>
+                    <section>
+                        <img className='border-t-2' src={post.postImageUrl} alt='Post image'/>
+                    </section>
+
+                    <section className='flex items-center gap-4 bg-black p-5 border-y-2'>
                         <img src='/heart.svg' alt='like button'/>
-                        <img src='/chat-quote.svg' alt='comment'/>
+                        <p>10</p>
+                        <img className='mb-1' src='/chat-quote.svg' alt='comment'/>
+                        <p>10</p>
+                    </section>
+
+                    <section className='bg-[#111111] p-5'>
+                        <div>
+                            <h3 className='text-white text-xl'>{profile.profileName}</h3>
+                            <p>{post.postContent}</p>
+                        </div>
+
+                        <div>
+
+                        </div>
                     </section>
                 </div>
             </main>
