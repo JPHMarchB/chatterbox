ab0ccfe95789fa16637be03472de8d8831ebe4ff
Joseph Marchbanks <153705879+JPHMarchB@users.noreply.github.com>
2024-04-16 20:59:12 -0600

    Began set up of post creation

--- frontend/src/app/(side-site)/create-post/page.tsx
+++ frontend/src/app/(side-site)/create-post/page.tsx
@@ -1,11 +1,20 @@
 import {JSX} from "react";
 import {Explore} from "@/app/(main-site)/explore/Explore";
+import {PostImageForm} from "@/app/(side-site)/create-post/PostImageForm";
+import {PostingForm} from "@/app/(side-site)/create-post/PostingForm";
+import {getSession} from "@/utils/fetchSession";
 
-export default function ExplorePage() : JSX.Element {
+export default async function ExplorePage() : Promise<JSX.Element> {
+    const session = await getSession()
+
+    if (!session) {return (<></>)}
+
+
     return (
         <>
             <main className='bg-[#222] p-5'>
-                <h1 className='text-xl text-white pb-3'>What up</h1>
+                {/*<PostingForm session={session} post={}/>*/}
+                {/*<PostImageForm session={session} post={}/>*/}
             </main>
         </>
     )
